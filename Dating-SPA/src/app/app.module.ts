96bdcffd59d5503bd7122d89ae644ef4eb421c23
bkishang <bguruvindapalli@gmail.com>
2019-08-22 15:52:05 -0400

    Initial commit

---
+++ Dating-SPA/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ValuesComponent } from './values/values.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValuesComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
